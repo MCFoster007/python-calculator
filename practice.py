666a48e6455d04bba4e3ee01704e46bccc0f2c96
mariechantalfoster@gmail.com <marie.foster@shu.edu>
2025-11-12 16:23:04 -0500

    Initial commit: calculator project

---
+++ practice.py
@@ -0,0 +1,8 @@
+print("let's practice python.")
+name =input("what's your name?")
+print(f"nice to meet you, {name}!")
+age = input("how old are you?")
+print(f"wow, {age} years old! that's great!")
+
+
+
